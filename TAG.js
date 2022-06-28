d0c8c21ffa4029e12bf72c485697923da1432be2
天空之城 <57618347+tkzc11@users.noreply.github.com>
2022-06-28 15:43:57 +0800

    Update TAG.js

--- TAG.js
+++ TAG.js
@@ -1,6 +1,6 @@
 function rename(nodes){
     let names = nodes.names;
-    names = $.replace(names, /越南家宽/, 越南-河内家宽);
+    names = $.replace(names, /越南家宽/, 越南河内家宽);
     return names;
     names = $.trim(names);
 }
