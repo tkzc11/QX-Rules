8fdb69fc56cb55286ee2134b0faca311bc9c5700
天空之城 <57618347+tkzc11@users.noreply.github.com>
2022-07-03 17:29:50 +0800

    Update Bilibili_AdBlock.js

--- Bilibili_AdBlock.js
+++ Bilibili_AdBlock.js
@@ -44,4 +44,4 @@
 # > Proto去广告
 ^https:\/\/app\.bilibili\.com\/bilibili\.app\.(view\.v1\.View\/View|dynamic\.v2\.Dynamic\/DynAll)$ url script-response-body https://raw.githubusercontent.com/app2smile/rules/master/js/bilibili-proto.js
 # > 动态广告
-;^https://app\.bilibili\.com/bilibili\.app\.dynamic\.v2\.Dynamic/DynAll$ url script-response-body https://raw.githubusercontent.com/yjqiang/surge_scripts/main/scripts/bilibili/bilibili_dynamic.js
+^https://app\.bilibili\.com/bilibili\.app\.dynamic\.v2\.Dynamic/DynAll$ url script-response-body https://raw.githubusercontent.com/yjqiang/surge_scripts/main/scripts/bilibili/bilibili_dynamic.js
