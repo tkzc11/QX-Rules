a7a940c6ae7adb72945a12cfc9fa6abd91f69db1
天空之城 <57618347+tkzc11@users.noreply.github.com>
2022-07-05 11:26:24 +0800

    Update AdBlock.js

--- AdBlock.js
+++ AdBlock.js
@@ -4,9 +4,9 @@ hostname=img-ys011.didistatic.com, sdkapp.uve.weibo.com, mapi.weibo.com, *.uve.w
 ^https?:\/\/img-ys011\.didistatic\.com\/static\/ad_oss\/.*.jpg url reject
 # >> 新浪微博
 ^https?:\/\/adimg\.uve\.weibo\.com\/public\/files\/image url reject
-^https?:\/\/sdkapp\.uve\.weibo\.com\/interface\/sdk\/(actionad|sdkad)\.php url reject-200
-^https?:\/\/wbapp\.uve\.weibo\.com\/wbapplua\/wbpullad\.lua url reject-200
-^https?:\/\/weibointl\.api\.weibo\.cn\/portal\.php\?a=get_coopen_ads url reject-200
+^https?:\/\/sdkapp\.uve\.weibo\.com\/interface\/sdk\/(actionad|sdkad)\.php url reject
+^https?:\/\/wbapp\.uve\.weibo\.com\/wbapplua\/wbpullad\.lua url reject
+^https?:\/\/weibointl\.api\.weibo\.cn\/portal\.php\?a=get_coopen_ads url reject
 ^https?:\/\/api\.weibo\.cn\/2\/statuses\/extend\?gsid= url reject
 ^https?:\/\/simg\.s\.weibo\.com\/.+?_ios\d{2}\.gif url reject-img
 ^https?:\/\/storage\.wax\.weibo\.com\/\w+\.(png|jpg|mp4) url reject-img
