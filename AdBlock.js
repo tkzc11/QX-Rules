de3f9c9644e21fa84fabe5ae6a6d6ec981b583c3
天空之城 <1540270882@qq.com>
2022-07-18 17:35:52 +0800

    Update AdBlock.js

--- AdBlock.js
+++ AdBlock.js
@@ -1,5 +1,5 @@
 # > Google
-^https?:\/\/(www.)?(g|google)\.cn url 302 google.com
+^https?://(www.)?(g|google).cn url 302 https://www.google.com/
 # > Netflix评分
 ^https?://ios(-.*)?\.prod\.ftl\.netflix\.com/iosui/user/.+path=%5B%22videos%22%2C%\d+%22%2C%22summary%22%5D url script-request-header https://raw.githubusercontent.com/yichahucha/surge/master/nf_rating.js
 ^https?://ios(-.*)?\.prod\.ftl\.netflix\.com/iosui/user/.+path=%5B%22videos%22%2C%\d+%22%2C%22summary%22%5D url script-response-body https://raw.githubusercontent.com/yichahucha/surge/master/nf_rating.js
