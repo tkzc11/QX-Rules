bd6c10f5fa958883fa0cfebb7a8f546d211e1a92
天空之城 <1540270882@qq.com>
2022-10-04 00:14:40 +0800

    Update Weibo.js

--- Weibo.js
+++ Weibo.js
@@ -1,3 +1,4 @@
+# > 微博其它
 ^https?:\/\/.+\/wbts\.png$ url reject-200
 ^https?:\/\/.+\/licai\.png$ url reject-200
 ^https?:\/\/.+\/3fc\.png$ url reject-200
@@ -12,36 +13,61 @@
 ^https?:\/\/h5\.sinaimg\.cn\/m\/taskcenter\/js\/index\.lazy\.+\.js$ url reject-200
 ^https?:\/\/captcha\.weibo\.com\/static\/js\/jigsawLock\.min\.js$ url reject-200
 ^https?:\/\/wx[0-9]\.sinaimg\.cn\/large\/.+\.gif$ url reject-200
-^https?:\/\/luck\.sc\.weibo\.com\/aj\/Action\/indexInfo$ url reject-dict
-^https?:\/\/new\.vip\.weibo\.c(n|om)\/aj\/vipcenter\/popalert url reject-dict
-^https?:\/\/new\.vip\.weibo\.c(n|om)\/aj\/vipcenter\/popalert url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/statuses\/unread_hot_timeline\?gsid= url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/device\/get url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/huati\/discovery_home_bottom_getdotinfo url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/huati\/discovery_home_bottom_channel_list url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/statuses\/unread_topic_timeline url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/statuses\/container_timeline_topic url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/carrier\/get\/info url reject-dict
-^https?:\/\/shop\.sc\.weibo\.com\/aj\/h5\/order\/prolist url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/redpacket url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/community_tab url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/multimedia\/playback\/batch_get url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/multimedia\/video\/comments\/get_comments_order_by_hot url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/tiny_stream_channel_list url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/tiny_stream_video_list url reject-dict
-^https?:\/\/letsgo\.e\.weibo\.com\/stracker\/fe\/report url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/multimedia\/video\/red_package\/grow_follower\/active_uids url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/client\/tabbar url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/push\/daily url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/page\/get_fragment url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/push\/active url reject-dict
-^https?:\/\/m\.weibo\.cn\/c\/checkin url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/machine url reject-dict
-^https?:\/\/fastimage\.uve\.weibo\.com\/interface\/sdk\/getad\.php$ url reject-dict
-^https?:\/\/sdkapp\.uve\.weibo\.com\/interface\/sdk\/actionpro\.php url reject-dict
-^https?:\/\/sdkclick\.uve\.weibo\.com\/interface\/sdk\/sdkclick\.php url reject-dict
-^https?:\/\/sdkapp\.uve\.weibo\.com\/interface\/sdk\/sdkconfig\.php url reject-dict
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/hot\/repost_daily url reject-dict
-(^https?:\/\/js\.t\.sinajs\.cn\/c2p\/sam\/frontend\/assets\/walletcache\.json|^https?:\/\/pay\.sc\.weibo\.com\/api\/client\/sdk\/app\/indexnew\/(index|person)|^https?:\/\/shop\.sc\.weibo\.com\/aj\/h5\/order\/center) url script-response-body https://hub.nange.cn/Script/Weibo/Wallet.js
+
+# > 微博积分弹窗
+^https?:\/\/luck\.sc\.weibo\.com\/aj\/Action\/indexInfo$ url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+
+# > 会员中心弹窗
+^https?:\/\/new\.vip\.weibo\.c(n|om)\/aj\/vipcenter\/popalert url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/new\.vip\.weibo\.c(n|om)\/aj\/vipcenter\/popalert url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+
+# > 微博推荐
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/statuses\/unread_hot_timeline\?gsid= url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+
+# > 获取设备型号
+^https?:\/\/m?api\.weibo\.c(n|om)\/device\/get url script-response-body https://hub.nange.cn/Mock/Weibo/getDevice.json
+
+# > 微博话题按钮红点
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/huati\/discovery_home_bottom_getdotinfo url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+
+# > 微博超话看帖
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/huati\/discovery_home_bottom_channel_list url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/statuses\/unread_topic_timeline url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/statuses\/container_timeline_topic url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+
+# > 微博订单
+^https?:\/\/shop\.sc\.weibo\.com\/aj\/h5\/order\/prolist url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+
+# > 微博视频推荐
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/redpacket url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/community_tab url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/multimedia\/playback\/batch_get url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/multimedia\/video\/comments\/get_comments_order_by_hot url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/tiny_stream_channel_list url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/tiny_stream_video_list url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+
+# > 微博其它
+^https?:\/\/letsgo\.e\.weibo\.com\/stracker\/fe\/report url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/multimedia\/video\/red_package\/grow_follower\/active_uids url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/client\/tabbar url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/push\/daily url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/page\/get_fragment url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/push\/active url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m\.weibo\.cn\/c\/checkin url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/machine url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/fastimage\.uve\.weibo\.com\/interface\/sdk\/getad\.php$ url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/sdkapp\.uve\.weibo\.com\/interface\/sdk\/actionpro\.php url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/sdkclick\.uve\.weibo\.com\/interface\/sdk\/sdkclick\.php url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/sdkapp\.uve\.weibo\.com\/interface\/sdk\/sdkconfig\.php url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+^https?:\/\/m?api\.weibo\.c(n|om)\/2\/hot\/repost_daily url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
+
+#开屏
 ^https?://(sdk|wb)app\.uve\.weibo\.com(/interface/sdk/sdkad.php|/wbapplua/wbpullad.lua) url script-response-body https://raw.githubusercontent.com/zmqcherish/proxy-script/main/weibo_launch.js
+
+#微博信息流
+^https?:\/\/m?api\.weibo\.c(n|om)/2/(statuses/(unread|extend|positives/get|(friends|video)(/|_)(mix)?timeline)|stories/(video_stream|home_list)|(groups|fangle)/timeline|profile/statuses|comments/build_comments|photo/recommend_list|service/picfeed|cardlist|page|!/(photos/pic_recommend_status|live/media_homelist|wbox\/.+\/createrIndex|st_videos\/tiny\/effect\/shoot_display_config)|video/tiny_stream_video_list|photo/info|remind/unread_count|search/finder|search/container_timeline|!\/huati\/discovery_home_bottom_channel_list|statuses\/unread_topic_timeline) url script-response-body https://hub.nange.cn/Script/Weibo/Timeline.js
+
+#微博钱包订单
+(^https?:\/\/js\.t\.sinajs\.cn\/c2p\/sam\/frontend\/assets\/walletcache\.json|^https?:\/\/pay\.sc\.weibo\.com\/api\/client\/sdk\/app\/indexnew\/(index|person)|^https?:\/\/shop\.sc\.weibo\.com\/aj\/h5\/order\/center) url script-response-body https://hub.nange.cn/Script/Weibo/Wallet.js
+
 hostname = api.weibo.cn, mapi.weibo.com, *.uve.weibo.com, new.vip.weibo.cn, h5.sinaimg.cn, *.sc.weibo.com, captcha.weibo.com, wx*.sinaimg.cn, js.t.sinajs.cn
