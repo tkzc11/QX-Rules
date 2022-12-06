ae87c64c0a3b962702a03b08f01064213ed2759c
天空之城 <1540270882@qq.com>
2022-12-06 23:07:08 +0800

    Create NotBoringPro.js

--- NotBoringPro.js
+++ NotBoringPro.js
@@ -10,7 +10,7 @@
   
 # ～ Not Boring 四件套[天气、习惯、日历、时间] 解锁会员权限（2022-12-03）@ddgksf2013
 ^https?:\/\/api-weather\.andy\.works\/v\d\/\w{13,18}$ url reject-dict
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/*) url script-echo-response NotBoringProCrack.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/*) url script-echo-response https://raw.githubusercontent.com/tkzc11/QX-Rules/main/NotBoringPro.js
 
 [mitm] 
 
