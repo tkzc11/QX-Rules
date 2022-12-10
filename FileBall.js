2542bf3313e38d5528f1bcc50c3030686128ab45
天空之城 <1540270882@qq.com>
2022-12-10 19:28:01 +0800

    Update FileBall.js

--- FileBall.js
+++ FileBall.js
@@ -9,8 +9,8 @@
 [rewrite_local]
   
 # ～ FileBall解锁会员权限（2022-12-08）@ddgksf2013
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/*) url script-echo-response FileBallProCrack.js
-^https?:\/\/firebaseremoteconfig\.googleapis\.com\/v\d\/projects\/filebox url script-echo-response FileBallProCrack.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/*) url script-echo-response https://raw.githubusercontent.com/tkzc11/QX-Rules/main/FileBall.js
+^https?:\/\/firebaseremoteconfig\.googleapis\.com\/v\d\/projects\/filebox url script-echo-response https://raw.githubusercontent.com/tkzc11/QX-Rules/main/FileBall.js
 
 [mitm] 
 
