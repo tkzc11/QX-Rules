553a1dcd908fe65f728f723ee248e78289495546
天空之城 <1540270882@qq.com>
2022-12-14 22:18:38 +0800

    Update Weibo.js

--- Weibo.js
+++ Weibo.js
@@ -1,73 +1,42 @@
-# > 微博其它
-^https?:\/\/.+\/wbts\.png$ url reject-200
-^https?:\/\/.+\/licai\.png$ url reject-200
-^https?:\/\/.+\/3fc\.png$ url reject-200
-^https?:\/\/.+\/pull-flv-(.*)\.douyincdn\.com\/stage\/.+\.flv url reject-200
-^https?:\/\/h5\.sinaimg\.cn\/upload\/.+\/NavigationBar_Video_Icon_Live_Normal\.png$ url reject-200
-^https?:\/\/.+\.sinaimg\.cn\/.+\/(.*)?(fish|redicon|redpacket)(.*)?\.(png|jpg|jpeg|gif)$ url reject-200
-^https?:\/\/.+\.sinaimg\.cn\/.+\/lipin_dongtu\.gif$ url reject-200
-^https?:\/\/h5\.sinaimg\.cn\/m\/taskcenter url reject-200
-^https?:\/\/h5\.sinaimg\.cn\/m\/taskcenter\/js\/vendor\.+\.js$ url reject-200
-^https?:\/\/h5\.sinaimg\.cn\/m\/taskcenter\/js\/app\.+\.js$ url reject-200
-^https?:\/\/h5\.sinaimg\.cn\/m\/taskcenter\/js\/0\.+\.js$ url reject-200
-^https?:\/\/h5\.sinaimg\.cn\/m\/taskcenter\/js\/index\.lazy\.+\.js$ url reject-200
-^https?:\/\/captcha\.weibo\.com\/static\/js\/jigsawLock\.min\.js$ url reject-200
-^https?:\/\/wx[0-9]\.sinaimg\.cn\/large\/.+\.gif$ url reject-200
+// @ScriptName        微博&微博国际版净化
+// @Author            @ddgksf2013,@zmqcherish,@shiro
+// @UpdateTime        2022-12-14
+// @Attention         微博、微博国际版净化
+// @Function          去广告配置
+// @Version           V2.0.31
+// @ScriptURL         https://github.com/ddgksf2013/Rewrite/raw/master/AdBlock/Weibo.conf
 
-# > 微博积分弹窗
-^https?:\/\/luck\.sc\.weibo\.com\/aj\/Action\/indexInfo$ url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
 
-# > 会员中心弹窗
-^https?:\/\/new\.vip\.weibo\.c(n|om)\/aj\/vipcenter\/popalert url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/new\.vip\.weibo\.c(n|om)\/aj\/vipcenter\/popalert url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-
-# > 微博推荐
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/statuses\/unread_hot_timeline\?gsid= url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-
-# > 获取设备型号
-^https?:\/\/m?api\.weibo\.c(n|om)\/device\/get url script-response-body https://hub.nange.cn/Mock/Weibo/getDevice.json
-
-# > 微博话题按钮红点
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/huati\/discovery_home_bottom_getdotinfo url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-
-# > 微博超话看帖
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/huati\/discovery_home_bottom_channel_list url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/statuses\/unread_topic_timeline url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/statuses\/container_timeline_topic url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-
-# > 微博订单
-^https?:\/\/shop\.sc\.weibo\.com\/aj\/h5\/order\/prolist url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-
-# > 微博视频推荐
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/redpacket url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/community_tab url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/multimedia\/playback\/batch_get url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/multimedia\/video\/comments\/get_comments_order_by_hot url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/tiny_stream_channel_list url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/tiny_stream_video_list url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-
-# > 微博其它
-^https?:\/\/letsgo\.e\.weibo\.com\/stracker\/fe\/report url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/!\/multimedia\/video\/red_package\/grow_follower\/active_uids url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/client\/tabbar url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/push\/daily url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/page\/get_fragment url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/push\/active url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m\.weibo\.cn\/c\/checkin url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/video\/machine url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/fastimage\.uve\.weibo\.com\/interface\/sdk\/getad\.php$ url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/sdkapp\.uve\.weibo\.com\/interface\/sdk\/actionpro\.php url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/sdkclick\.uve\.weibo\.com\/interface\/sdk\/sdkclick\.php url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/sdkapp\.uve\.weibo\.com\/interface\/sdk\/sdkconfig\.php url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-^https?:\/\/m?api\.weibo\.c(n|om)\/2\/hot\/repost_daily url script-response-body https://hub.nange.cn/Mock/Blank/Blank_Dict.json
-
-#开屏
-^https?://(sdk|wb)app\.uve\.weibo\.com(/interface/sdk/sdkad.php|/wbapplua/wbpullad.lua) url script-response-body https://raw.githubusercontent.com/zmqcherish/proxy-script/main/weibo_launch.js
-
-#微博信息流
-^https?:\/\/m?api\.weibo\.c(n|om)/2/(statuses/(unread|extend|positives/get|(friends|video)(/|_)(mix)?timeline)|stories/(video_stream|home_list)|(groups|fangle)/timeline|profile/statuses|comments/build_comments|photo/recommend_list|service/picfeed|cardlist|page|!/(photos/pic_recommend_status|live/media_homelist|wbox\/.+\/createrIndex|st_videos\/tiny\/effect\/shoot_display_config)|video/tiny_stream_video_list|photo/info|remind/unread_count|search/finder|search/container_timeline|!\/huati\/discovery_home_bottom_channel_list|statuses\/unread_topic_timeline) url script-response-body https://hub.nange.cn/Script/Weibo/Timeline.js
-
-#微博钱包订单
-(^https?:\/\/js\.t\.sinajs\.cn\/c2p\/sam\/frontend\/assets\/walletcache\.json|^https?:\/\/pay\.sc\.weibo\.com\/api\/client\/sdk\/app\/indexnew\/(index|person)|^https?:\/\/shop\.sc\.weibo\.com\/aj\/h5\/order\/center) url script-response-body https://hub.nange.cn/Script/Weibo/Wallet.js
-
-hostname = api.weibo.cn, mapi.weibo.com, *.uve.weibo.com, new.vip.weibo.cn, h5.sinaimg.cn, *.sc.weibo.com, captcha.weibo.com, wx*.sinaimg.cn, js.t.sinajs.cn
+hostname = *api.weibo*, *uve.weibo.com, new.vip.weibo.cn
+# > 微博_自定义tab皮肤@zmqcherish
+^https://api.weibo.cn/2/!/client/light_skin url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
+# > 微博_非会员设置tab皮肤@zmqcherish
+^https://new.vip.weibo.cn/littleskin/preview url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
+# > 微博_去广告以及去除各部分推广模块@zmqcherish
+^https?:\/\/m?api\.weibo\.c(n|om)\/\d\/(searchall|page\?|messageflow) url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
+^https?:\/\/m?api\.weibo\.c(n|om)\/\d\/statuses/(unread_)?friends(/|_)timeline url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
+^https?:\/\/m?api\.weibo\.c(n|om)\/\d\/groups/timeline url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
+^https?:\/\/m?api\.weibo\.c(n|om)\/\d\/statuses/(container_timeline|unread_hot_timeline|extend|video_mixtimeline|unread_topic_timeline) url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
+^https?:\/\/m?api\.weibo\.c(n|om)\/\d\/profile/(me|container_timeline) url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
+^https?:\/\/m?api\.weibo\.c(n|om)\/\d\/video/(community_tab|remind_info|tiny_stream_video_list) url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
+^https?:\/\/m?api\.weibo\.c(n|om)\/\d\/search/(finder|container_timeline|container_discover) url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
+^https?:\/\/m?api\.weibo\.c(n|om)\/\d\/(checkin/show|\!/live/media_homelist|comments/build_comments|container/get_item) url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
+^https?:\/\/m?api\.weibo\.c(n|om)\/\d\/cardlist url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
+# > 微博国际版_hot_search@ddgksf2013
+^https?:\/\/weibointl\.api\.weibo\.c(n|om)\/portal\.php\?a=hot_search_users url reject-dict
+# > 微博国际版_热推荐@ddgksf2013
+^https?:\/\/m?api\.weibo\.c(n|om)\/\d\/ad\/weibointl\? url reject-dict
+# > 微博国际版_屏蔽search_suggest@ddgksf2013
+# ^https?:\/\/m?api\.weibo\.c(n|om)\/\d\/page\/get_search_suggest url reject-dict
+# > 微博国际版_屏蔽searching_info@ddgksf2013
+^https?:\/\/weibointl\.api\.weibo\.c(n|om)\/portal\.php.*user&a=get_searching_info url echo-response text/html echo-response https://github.com/ddgksf2013/Scripts/raw/master/weibo_search_info.json
+# > 微博国际版_屏蔽search_topic@ddgksf2013 
+^https?:\/\/weibointl\.api\.weibo\.c(n|om)\/portal\.php.*feed&a=search_topic url echo-response text/html echo-response https://github.com/ddgksf2013/Scripts/raw/master/weibo_search_topic.json
+# > 微博国际版_屏蔽开屏广告@ddgksf2013 
+^https?:\/\/weibointl\.api\.weibo\.c(n|om)\/portal\.php.*get_coopen_ads url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
+# > 微博&国际版_sdkad@ddgksf2013
+^https?://(sdk|wb)app\.uve\.weibo\.com(/interface/sdk/sdkad.php|/wbapplua/wbpullad.lua) url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
+# > 微博国际版_趋势顶部CARD去广告@ddgksf2013
+^https?:\/\/weibointl\.api\.weibo\.c(n|om)\/portal\.php.*feed&a=trends url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
+# > 微博国际版_用户中心@ddgksf2013
+^https?:\/\/weibointl\.api\.weibo\.cn\/portal\.php\?a=user_center url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/weibo_json.js
