2dfa804e684737fe8d98b8334a8dc0ccc4cde3be
天空之城 <1540270882@qq.com>
2023-01-22 03:05:10 +0800

    Create Nicegram.js

---
+++ Nicegram.js
@@ -0,0 +1,9 @@
+hostname = restore-access.indream.app
+
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://github.com/ddgksf2013/dev/raw/main/NicegramProCrack.js
+
+{
+  "data": {
+    "premiumAccess": true
+  }
+}
