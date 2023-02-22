b5c73fbb469dc07f4c62a80cdfa92d8b3d1a7ca2
天空之城 <1540270882@qq.com>
2023-02-22 15:15:37 +0800

    Update 12306.js

--- 12306.js
+++ 12306.js
@@ -15,6 +15,6 @@ hostname = ad.12306.cn
 
 ***********************************************/
 
-const version = 'V1.0.19';
+const version = 'V1.0.20';
 
-var ddgksf2013=JSON.parse($response.body);ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"ddgksf2013",creativeType:1}],ddgksf2013.advertParam={skipTime:1},delete ddgksf2013.message,$done({body:JSON.stringify(ddgksf2013)});
+var obj=JSON.parse($request.body),ddgksf2013={};"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"ddgksf2013",creativeType:1}],ddgksf2013.advertParam={skipTime:1},ddgksf2013.code="00"):ddgksf2013={code:"00",message:"无广告返回"},$done({body:JSON.stringify(ddgksf2013)});
