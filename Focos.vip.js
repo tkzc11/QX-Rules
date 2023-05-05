79d8569ef68680352dc180068bf1eed5223b0d46
天空之城 <1540270882@qq.com>
2023-05-05 23:59:10 +0800

    Update Focos.vip.js

--- Focos.vip.js
+++ Focos.vip.js
@@ -1,3 +1,3 @@
 # > focos☆解锁会员权限（2023-05-05）@ddgksf2013
-^https?:\/\/focos\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body focos.vip.js 
+^https?:\/\/focos\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify\/apple) url script-response-body https://raw.githubusercontent.com/tkzc11/QX-Rules/main/Focos.vip.js
 hostname = focos.oracle.bendingspoonsapps.com
