647428135b7179e3007038d95a7ed5789940fab4
天空之城 <57618347+tkzc11@users.noreply.github.com>
2023-07-23 20:46:32 +0800

    Create New Bing.js

---
+++ New Bing.js
@@ -0,0 +1,2 @@
+https:\/\/www\.bing\.com\/search url request-header (.+\r\n)User-Agent "Mozilla/5.0 (iphone; Intel Mac OS X 10_12_6) AppleWebKit/537.36 Chrome/110.0 Safari/537.36 Edg/110.0":.+(\r\n) request-header $1User-Agent "Mozilla/5.0 (iphone; Intel Mac OS X 10_12_6) AppleWebKit/537.36 Chrome/110.0 Safari/537.36 Edg/110.0"$2
+hostname = www.bing.com
