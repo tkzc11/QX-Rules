79cd2c344799028472828678b948c9ea0e1c598e
天空之城 <57618347+tkzc11@users.noreply.github.com>
2023-09-25 18:00:31 +0800

    Update reddit.js

--- reddit.js
+++ reddit.js
@@ -1,25 +1,30 @@
-/***********************************
+// 2023-07-23 16:15
 
-[rewrite_local]
-^https:\/\/oauth\.reddit\.com\/api\/v\d\/me\.json url script-response-body https://raw.githubusercontent.com/tkzc11/QX-Rules/main/Reddit.js
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
 
-[mitm] 
+if (obj.data) {
+  if (obj.data?.home?.elements?.edges) {
+    // Home
+    obj.data.home.elements.edges = obj.data.home.elements.edges.filter(
+      (i) => !i?.node?.__typename?.includes("AdPost")
+    );
+  } else if (obj.data?.popularV3?.elements?.edges) {
+    // Popular
+    obj.data.popularV3.elements.edges =
+      obj.data.popularV3.elements.edges.filter(
+        (i) => i?.node?.adPayload === null
+      );
+  } else if (obj.data?.subredditInfoByName?.elements?.edges) {
+    obj.data.subredditInfoByName.elements.edges =
+      obj.data.subredditInfoByName.elements.edges.filter(
+        (i) => !i?.node?.__typename?.includes("AdPost")
+      );
+  } else if (obj.data?.subredditsInfoByNames) {
+    obj.data.subredditsInfoByNames = obj.data.subredditsInfoByNames.map(
+      (i) => ({ ...i, isNsfw: false })
+    );
+  }
+}
 
-hostname = oauth.reddit.com
-
-***********************************/
-
-var obj = JSON.parse($response.body);
-obj['has_gold_subscription'] = true;
-obj['pref_autoplay'] = false;
-obj['has_subscribed_to_premium'] = true;
-obj['has_visited_new_profile'] = true;
-obj['pref_video_autoplay'] = false;
-obj['features']['promoted_trend_blanks'] = false;
-obj['is_mod'] = true;
-obj['is_gold'] = true;
-obj['has_ios_subscription'] = true;
-obj['seen_premium_adblock_modal'] = true;
-obj['can_edit_name'] = true;
-obj['has_external_account'] = true;
-$done({body: JSON.stringify(obj)});
+$done({ body: JSON.stringify(obj) });
