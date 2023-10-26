541448ef296711b0dbe90793fa04b2ec99349da7
天空之城 <57618347+tkzc11@users.noreply.github.com>
2023-10-26 18:54:24 +0800

    Update AdBlock.js

--- AdBlock.js
+++ AdBlock.js
@@ -154,8 +154,9 @@ https?:\/\/awg\.enmonster\.com\/apa\/(advert\/demand\/home\/poster|index\/advert
 ^https?:\/\/gfp\.veta\.naver\.com\/adcall\? url reject
 ^https?:\/\/api\.hanju\.koudaibaobao\.com\/api\/carp\/kp\? url reject
 # > 航旅纵横
-^http:\/\/(discardrp|startup)\.umetrip\.com\/gateway\/api\/umetrip\/native url reject
-^http:\/\/(114\.115\.217\.129)|(home\.umetrip\.com)\/gateway\/api\/umetrip\/native$ url script-response-header https://github.com/ddgksf2013/Scripts/raw/master/hanglvzongheng.js
+^http:\/\/discardrp\.umetrip\.com\/gateway\/api\/umetrip\/native url reject-200
+^http:\/\/oss\.umetrip\.com\/fs\/advert\/polite url reject
+^https?\/\/(114\.115\.217\.129)|(home\.umetrip\.com)\/gateway\/api\/umetrip\/native$ url script-response-header https://github.com/ddgksf2013/Scripts/raw/master/hanglvzongheng.js
 # > 和风天气
 ^https:\/\/hfapp-service\.qweather\.net\/v2\.0\/app\/ad\/list\? url reject
 # > 盒马
